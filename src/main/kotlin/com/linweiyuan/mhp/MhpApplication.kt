3263f879ac9a1815a2caa707eb8a22ae6e15fd71
linweiyuan <root@linweiyuan.com>
2019-10-01 14:14:22 +0800

    Init

---
+++ src/main/kotlin/com/linweiyuan/mhp/MhpApplication.kt
@@ -0,0 +1,11 @@
+package com.linweiyuan.mhp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MhpApplication
+
+fun main(args: Array<String>) {
+    runApplication<MhpApplication>(*args)
+}
